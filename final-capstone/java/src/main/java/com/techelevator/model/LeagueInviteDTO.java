153246bb342362f22fdbdc7728a2ec3e8c9cc66e
Brian Kissinger <bikissinger@gmail.com>
2021-12-08 09:48:03 -0500

    Submitting UserId method

---
+++ final-capstone/java/src/main/java/com/techelevator/model/LeagueInviteDTO.java
@@ -0,0 +1,9 @@
+package com.techelevator.model;
+
+public class LeagueInviteDTO {
+    private Long status;
+    private Long memberId;
+    private Long leagueId;
+
+    public LeagueInviteDTO() {}
+}
