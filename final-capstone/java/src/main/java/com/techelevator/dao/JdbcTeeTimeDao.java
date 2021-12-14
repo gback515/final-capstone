af69a01ab086e2986423bbf0de9c8159d6d5095d
Gavin Back <gback515@gmail.com>
2021-12-14 15:16:15 -0500

    changed some names

--- final-capstone/java/src/main/java/com/techelevator/dao/JdbcTeeTimeDao.java
+++ final-capstone/java/src/main/java/com/techelevator/dao/JdbcTeeTimeDao.java
@@ -18,7 +18,7 @@ public class JdbcTeeTimeDao implements TeeTimeDao {
     @Override
     public List<TeeTime> findAll() {
         List<TeeTime> teeTimes = new ArrayList<>();
-        String sql = "SELECT tee_time_id, user_id, course_id, tee_time_date, tee_time FROM tee_times;";
+        String sql = "SELECT tee_time_id, user_id, league_id, tee_time_date, tee_time FROM tee_times;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         while (results.next()) {
             TeeTime teeTime = mapRowToTeeTime(results);
@@ -30,7 +30,7 @@ public class JdbcTeeTimeDao implements TeeTimeDao {
     @Override
     public List<TeeTime> findTeeTimeByUser(long userId) {
         List<TeeTime> teeTimes = new ArrayList<>();
-        String sql = "SELECT tee_time_id, user_id, course_id, tee_time_date, tee_time FROM tee_times WHERE user_id = ?;";
+        String sql = "SELECT tee_time_id, user_id, league_id, tee_time_date, tee_time FROM tee_times WHERE user_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql,userId);
         while (results.next()) {
             TeeTime teeTime = mapRowToTeeTime(results);
@@ -42,7 +42,7 @@ public class JdbcTeeTimeDao implements TeeTimeDao {
     @Override
     public TeeTime create(long userId, long courseId, String teeTimeDate, String time) {
         TeeTime teeTime = new TeeTime(userId, courseId, teeTimeDate, time);
-        String sql = "INSERT INTO tee_times (tee_time_id, user_id, course_id, tee_time_date, tee_time) VALUES(?,?,?,?,?) RETURNING tee_time_id";
+        String sql = "INSERT INTO tee_times (tee_time_id, user_id, league_id, tee_time_date, tee_time) VALUES(?,?,?,?,?) RETURNING tee_time_id";
         Long newTeeTimeId = jdbcTemplate.queryForObject(sql, Long.class, userId, courseId, teeTimeDate, time);
         teeTime.setTeeTimeId(newTeeTimeId);
         return teeTime;
@@ -53,7 +53,7 @@ public class JdbcTeeTimeDao implements TeeTimeDao {
         TeeTime teeTime = new TeeTime();
         teeTime.setTeeTimeId(results.getLong("tee_time_id"));
         teeTime.setUserId(results.getLong("user_id"));
-        teeTime.setCourseId(results.getLong("course_id"));
+        teeTime.setLeagueId(results.getLong("league_id"));
         teeTime.setTeeTimeDate(results.getString("tee_time_date"));
         teeTime.setTime(results.getString("tee_time"));
         return teeTime;
