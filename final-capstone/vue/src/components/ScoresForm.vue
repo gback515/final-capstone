3a23a2cbe7f0bd1ccb4624d3451c553770df0683
Andrew Holderman <andrewholderman@gmail.com>
2021-12-15 16:39:30 -0500

    Andrew Changes

--- final-capstone/vue/src/components/ScoresForm.vue
+++ final-capstone/vue/src/components/ScoresForm.vue
@@ -48,6 +48,8 @@ export default {
   methods: {
     submitScore() {
       const newScore = {
+        user_id: this.score.user_id,
+        tee_time_id: this.score.tee_time_id,
         scores: this.score.score,
         // score: parseInt(this.scores.score),
       };
